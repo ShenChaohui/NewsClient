8001c4cba3cc62bdb91433c319bb8ea600af1dad
ShenChenhui <30425939+ShenChenhui@users.noreply.github.com>
2018-02-05 11:41:14 +0800

    添加天气

--- app/src/main/java/com/geniuses/newsclient/activity/MainActivity.java
+++ app/src/main/java/com/geniuses/newsclient/activity/MainActivity.java
@@ -111,17 +111,10 @@ public class MainActivity extends AppCompatActivity
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_camera) {
-            // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
+        if (id == R.id.nav_weather) {
+            startActivity(new Intent(MainActivity.this, WeatherActivity.class));
 
-        } else if (id == R.id.nav_slideshow) {
-
-        } else if (id == R.id.nav_manage) {
-
-        } else if (id == R.id.nav_share) {
-
-        } else if (id == R.id.nav_send) {
+        } else if (id == R.id.nav_chat) {
             startActivity(new Intent(MainActivity.this, ChatbotActivity.class));
         }
 
---
+++ app/src/main/java/com/geniuses/newsclient/activity/WeatherActivity.java
@@ -0,0 +1,25 @@
+package com.geniuses.newsclient.activity;
+
+import com.geniuses.newsclient.R;
+
+/**
+ * Created by Sch on 2018/2/5.
+ */
+
+public class WeatherActivity extends BasicActivity {
+    @Override
+    public int getActivity() {
+        return R.layout.activity_weather;
+    }
+
+    @Override
+    protected void initView() {
+        initTitle();
+
+    }
+
+    @Override
+    protected void main() {
+
+    }
+}
