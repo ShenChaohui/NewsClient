b4d77f3a5fd840b2476064714d463301255246af
ShenChenhui <30425939+ShenChenhui@users.noreply.github.com>
2018-02-06 09:40:17 +0800

    添加城市选择器

--- app/src/main/java/com/geniuses/newsclient/activity/WeatherActivity.java
+++ app/src/main/java/com/geniuses/newsclient/activity/WeatherActivity.java
@@ -1,12 +1,19 @@
 package com.geniuses.newsclient.activity;
 
+import android.content.Intent;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.Toast;
+
 import com.geniuses.newsclient.R;
+import com.zaaach.citypicker.CityPickerActivity;
 
 /**
  * Created by Sch on 2018/2/5.
  */
 
 public class WeatherActivity extends BasicActivity {
+    private static final int REQUEST_CODE_PICK_CITY = 0;
     @Override
     public int getActivity() {
         return R.layout.activity_weather;
@@ -15,11 +22,26 @@ public class WeatherActivity extends BasicActivity {
     @Override
     protected void initView() {
         initTitle();
-
+        getRightButton(R.mipmap.ic_add).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivityForResult(new Intent(WeatherActivity.this, CityPickerActivity.class),
+                        REQUEST_CODE_PICK_CITY);
+            }
+        });
     }
 
     @Override
     protected void main() {
 
+    }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == REQUEST_CODE_PICK_CITY && resultCode == RESULT_OK) {
+            if (data != null) {
+                String city = data.getStringExtra(CityPickerActivity.KEY_PICKED_CITY);
+                Toast.makeText(this,city,Toast.LENGTH_SHORT).show();
+            }
+        }
     }
 }
--- app/src/main/java/com/geniuses/newsclient/util/GlobalValue.java
+++ app/src/main/java/com/geniuses/newsclient/util/GlobalValue.java
@@ -6,8 +6,8 @@ package com.geniuses.newsclient.util;
 
 public class GlobalValue {
     public static final String APPKEY = "66177f7bd8031630351e5463691837ba";
-    public static final String NEWS = "https://way.jd.com/jisuapi/get";
-    public static final String WEATHER = "https://way.jd.com/he/freeweather";
-    public static final String CHATBOX = "https://way.jd.com/turing/turing";
+    public static final String NEWS = "https://way.jd.com/jisuapi/get";//新闻
+    public static final String WEATHER = "https://way.jd.com/he/freeweather";//天气
+    public static final String CHATBOX = "https://way.jd.com/turing/turing";//图灵聊天机器人
 
 }
